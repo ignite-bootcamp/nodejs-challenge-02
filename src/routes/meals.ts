f19ee425c7b9449d32023c6a17683ca5d42fafa2
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-22 21:28:44 -0300

    feat: created route to edit meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -75,6 +75,47 @@ export async function mealsRoutes(app: FastifyInstance) {
     return reply.status(201).send()
   })
 
+  app.put(
+    '/:id',
+    { preHandler: [checkIfSessionExist] },
+    async (request, reply) => {
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.coerce.date(),
+        isOnDiet: z.boolean(),
+      })
+      const updateMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const body = updateMealBodySchema.parse(request.body)
+      const { id } = updateMealParamsSchema.parse(request.params)
+      const { userId } = request.cookies
+
+      const meal = await prisma.meal.findFirst({
+        where: {
+          id,
+        },
+      })
+
+      if (userId !== meal?.userId) {
+        return reply.status(401).send({
+          error: 'Unauthorized',
+        })
+      }
+
+      await prisma.meal.update({
+        data: body,
+        where: {
+          id,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+
   app.delete(
     '/:id',
     { preHandler: [checkIfSessionExist] },
