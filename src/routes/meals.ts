4cbc2e08fa193271d1ca378a0c15121d1298bced
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-22 21:21:37 -0300

    feat: created delete meal route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -74,4 +74,37 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.delete(
+    '/:id',
+    { preHandler: [checkIfSessionExist] },
+    async (request, reply) => {
+      const deleteMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = deleteMealParamsSchema.parse(request.params)
+      const { userId } = request.cookies
+
+      const meal = await prisma.meal.findFirst({
+        where: {
+          id,
+        },
+      })
+
+      if (userId !== meal?.userId) {
+        return reply.status(401).send({
+          error: 'Unauthorized',
+        })
+      }
+
+      await prisma.meal.delete({
+        where: {
+          id,
+        },
+      })
+
+      return reply.status(204).send()
+    },
+  )
 }
