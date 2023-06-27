410324ee8a19739786d79e167383a9a1a921d6b3
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-27 16:30:20 -0300

    tests: adding tests to list specific meal

--- src/test/meals.spec.ts
+++ src/test/meals.spec.ts
@@ -59,7 +59,7 @@ describe('/meals', () => {
       .set('Cookie', cookies)
 
     expect(response.body.meals).toEqual([
-      {
+      expect.objectContaining({
         id: expect.any(String),
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
@@ -68,7 +68,116 @@ describe('/meals', () => {
         description: 'very good hamburguer',
         date: mealDate,
         isOnDiet: false,
-      },
+      }),
     ])
+  })
+
+  it('should list specific meal', async () => {
+    const mealDate = new Date().toISOString()
+    const createResponse = await supertest(app.server).post('/meals').send({
+      name: 'Hamburguer',
+      description: 'very good hamburguer',
+      date: mealDate,
+      isOnDiet: false,
+    })
+
+    const cookies = createResponse.get('Set-Cookie')
+
+    const mealsResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+
+    const mealId = mealsResponse.body.meals[0].id
+
+    const specificMeal = await supertest(app.server)
+      .get(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+
+    expect(specificMeal.body.meal).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        userId: expect.any(String),
+        name: 'Hamburguer',
+        description: 'very good hamburguer',
+        date: mealDate,
+        isOnDiet: false,
+      }),
+    )
+  })
+
+  it('should update a meal', async () => {
+    const mealDate = new Date().toISOString()
+    const createMealResponse = await supertest(app.server).post('/meals').send({
+      name: 'Hamburguer',
+      description: 'very good hamburguer',
+      date: mealDate,
+      isOnDiet: false,
+    })
+
+    const cookies = createMealResponse.get('Set-Cookie')
+
+    const mealResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+    const mealId = mealResponse.body.meals[0].id
+    const oldUpdatedAt = mealResponse.body.meals[0].updatedAt
+
+    await supertest(app.server)
+      .put(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pizza',
+        description: 'very good pizza',
+        date: mealDate,
+        isOnDiet: true,
+      })
+
+    const newMealResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+    const newUpdatedAt = newMealResponse.body.meals[0].updatedAt
+
+    expect(oldUpdatedAt).not.toEqual(newUpdatedAt)
+    expect(newMealResponse.body.meals).toEqual([
+      expect.objectContaining({
+        id: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        userId: expect.any(String),
+        name: 'Pizza',
+        description: 'very good pizza',
+        date: mealDate,
+        isOnDiet: true,
+      }),
+    ])
+  })
+
+  it('should delete a meal', async () => {
+    const mealDate = new Date().toISOString()
+    const createMealResponse = await supertest(app.server).post('/meals').send({
+      name: 'Hamburguer',
+      description: 'very good hamburguer',
+      date: mealDate,
+      isOnDiet: false,
+    })
+
+    const cookies = createMealResponse.get('Set-Cookie')
+
+    const mealResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+    const mealId = mealResponse.body.meals[0].id
+
+    await supertest(app.server)
+      .delete(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+
+    const emptyListing = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+
+    expect(emptyListing.body.meals).toHaveLength(0)
   })
 })
